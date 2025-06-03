18f7f5b6638eba876932dc9ab01048d41f84e639
OfflcerSam <samuelthetank1@gmail.com>
2025-06-03 15:49:40 -0400

    starting relation tweak
    also more soviet groundwork

--- src/org/officersam/tanks/scripts/world/Tank_gen.java
+++ src/org/officersam/tanks/scripts/world/Tank_gen.java
@@ -21,11 +21,16 @@ public class Tank_gen implements SectorGeneratorPlugin {
 	
         new USA_StarofAmerica().generate(sector);
         new GER_SonneDeutschlands().generate(sector);
+        //new SOV_ZvezdaRodiny().generate(sector);
 		
         SharedData.getData().getPersonBountyEventData().addParticipatingFaction("USA");
+        SharedData.getData().getPersonBountyEventData().addParticipatingFaction("GER");
+        //SharedData.getData().getPersonBountyEventData().addParticipatingFaction("SOV");
+
 
         FactionAPI usa = sector.getFaction("USA");
         FactionAPI ger = sector.getFaction("GER");
+        //FactionAPI sov = sector.getFaction("SOV");
 
         FactionAPI player = sector.getFaction(Factions.PLAYER);
         FactionAPI hegemony = sector.getFaction(Factions.HEGEMONY);
@@ -39,34 +44,52 @@ public class Tank_gen implements SectorGeneratorPlugin {
 		FactionAPI persean = sector.getFaction(Factions.PERSEAN);
         FactionAPI guard = sector.getFaction(Factions.LIONS_GUARD);
 
-        usa.setRelationship(ger.getId(), -0.6f);
-        ger.setRelationship(usa.getId(), -0.6f);
+        usa.setRelationship(ger.getId(), -0.3f);
+        //usa.setRelationship(sov.getId(), 0.0f);
+
+        ger.setRelationship(usa.getId(), -0.3f);
+        //ger.setRelationship(sov.getId(), -0.4f);
+
+        //sov.setRelationship(usa.getId(), 0.0f);
+        //sov.setRelationship(ger.getId(), -0.3f);
 
 
 
-        usa.setRelationship(player.getId(), 0.3f);	
-        usa.setRelationship(hegemony.getId(), -0.2f);
+        usa.setRelationship(player.getId(), 0.1f);
+        usa.setRelationship(hegemony.getId(), 0.0f);
         usa.setRelationship(tritachyon.getId(), 0.2f);
-        usa.setRelationship(pirates.getId(), -0.9f);
+        usa.setRelationship(pirates.getId(), -1.0f);
         usa.setRelationship(independent.getId(), 0.6f);
-        usa.setRelationship(persean.getId(), 0.15f);
+        usa.setRelationship(persean.getId(), 0.2f);
         usa.setRelationship(church.getId(), -0.3f);
-        usa.setRelationship(path.getId(), -0.9f);
+        usa.setRelationship(path.getId(), -1.0f);
         usa.setRelationship(kol.getId(), 0.1f);
-        usa.setRelationship(diktat.getId(), -0.15f);
-        usa.setRelationship(guard.getId(), -0.25f);
+        usa.setRelationship(diktat.getId(), 0.1f);
+        usa.setRelationship(guard.getId(), -0.2f);
 
-        ger.setRelationship(player.getId(), 0.3f);
-        ger.setRelationship(hegemony.getId(), -0.6f);
-        ger.setRelationship(tritachyon.getId(), -0.2f);
-        ger.setRelationship(pirates.getId(), -0.9f);
-        ger.setRelationship(independent.getId(), 0.3f);
-        ger.setRelationship(persean.getId(), -0.2f);
+        ger.setRelationship(player.getId(), 0.0f);
+        ger.setRelationship(hegemony.getId(), -0.4f);
+        ger.setRelationship(tritachyon.getId(), 0.0f);
+        ger.setRelationship(pirates.getId(), -1.0f);
+        ger.setRelationship(independent.getId(), 0.1f);
+        ger.setRelationship(persean.getId(), 0.0f);
         ger.setRelationship(church.getId(), -0.3f);
-        ger.setRelationship(path.getId(), -0.9f);
+        ger.setRelationship(path.getId(), -1.0f);
         ger.setRelationship(kol.getId(), -0.2f);
-        ger.setRelationship(diktat.getId(), -0.4f);
+        ger.setRelationship(diktat.getId(), -0.3f);
         ger.setRelationship(guard.getId(), -0.4f);
+
+//        sov.setRelationship(player.getId(), 0.1f);
+//        sov.setRelationship(hegemony.getId(), 0.1f);
+//        sov.setRelationship(tritachyon.getId(), 0.0f);
+//        sov.setRelationship(pirates.getId(), -1.0f);
+//        sov.setRelationship(independent.getId(), 0.6f);
+//        sov.setRelationship(persean.getId(), 0.2f);
+//        sov.setRelationship(church.getId(), -0.3f);
+//        sov.setRelationship(path.getId(), -1.0f);
+//        sov.setRelationship(kol.getId(), 0.1f);
+//        sov.setRelationship(diktat.getId(), 0.1f);
+//        sov.setRelationship(guard.getId(), -0.2f);
 
     }
 }
